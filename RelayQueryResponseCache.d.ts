2d0904521691202cd77e6bbd9424c9c416c0d394
Matt Krick <matt.krick@gmail.com>
2019-10-31 11:20:04 -0600

    bump to v7

--- RelayQueryResponseCache.d.ts
+++
@@ -1,9 +0,0 @@
-import { Variables } from "./RelayRuntimeTypes"
-import { GraphQLResponse } from "./RelayNetworkTypes"
-
-export class QueryResponseCache {
-    constructor(options: { size: number; ttl: number })
-    clear(): void
-    get(queryID: string, variables: Variables): GraphQLResponse | null
-    set(queryID: string, variables: Variables, payload: GraphQLResponse): void
-}
--- commitLocalUpdate.d.ts
+++
@@ -1,5 +0,0 @@
-import { IEnvironment, StoreUpdater } from "./RelayStoreTypes"
-
-export function commitLocalUpdate(environment: IEnvironment, updater: StoreUpdater): void
-
-export { commitLocalUpdate as CLU }
